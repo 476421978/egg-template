90579256c62a97568d887722cba25622cf8707ae
huangligen <476421978@qq.com>
2021-09-02 17:23:32 +0800

    取点敏感信息

--- test/controller/home.test.js
+++ test/controller/home.test.js
@@ -3,23 +3,25 @@ const { app, mock, assert } = require('egg-mock/bootstrap')
 const fs = require('fs')
 describe('test/controller/home.test.js', () => {
   // 测试扩展方法
-  it('should get a ctx', async () => {
-    const ctx = app.mockContext()
-    const payConfig = new ctx.helper.WechatPay({
-      appId: '123',
-      mchId: '456',
-      publicKey: fs.readFileSync(`public/apiclient_cert.pem`),
-      privateKey: fs.readFileSync(`public/apiclient_key.pem`)
-    })
-    const wechatPay = await payConfig.WechatPayPost({
-      openId: '789',
-      description: '描述',
-      attach: '自定义数据',
-      total: 1,
-      notifyUrl: 'https://www.googlle.com'
-    })
-    console.log('wechatPay--->>>', wechatPay)
-  })
+  // it('should get a ctx', async () => {
+  //   const ctx = app.mockContext()
+  //   // 需要上传.pem文件到public
+  //   const { appId, mchId } = app.config.wechatInfo
+  //   const payConfig = new ctx.helper.WechatPay({
+  //     appId: appId,
+  //     mchId: mchId,
+  //     publicKey: fs.readFileSync(`public/apiclient_cert.pem`),
+  //     privateKey: fs.readFileSync(`public/apiclient_key.pem`)
+  //   })
+  //   const wechatPay = await payConfig.WechatPayPost({
+  //     openId: '789',
+  //     description: '描述',
+  //     attach: '自定义数据',
+  //     total: 1,
+  //     notifyUrl: 'https://www.googlle.com'
+  //   })
+  //   console.log('wechatPay--->>>', wechatPay)
+  // })
 
   // 测试文件上传
   // it('should post upload', async () => {
