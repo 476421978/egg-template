dfda2157f96c68ad9d7fd8f9db24223efe8d61be
huangligen <476421978@qq.com>
2021-09-02 17:18:27 +0800

    更正

--- app/utils/wechat/WXBizDataCrypt.js
+++ app/utils/wechat/WXBizDataCrypt.js
@@ -1,35 +1,32 @@
-const crypto = require('crypto');
+const crypto = require('crypto')
 
 function WXBizDataCrypt(appId, sessionKey) {
-  this.appId = appId;
-  this.sessionKey = sessionKey;
+  this.appId = appId
+  this.sessionKey = sessionKey
 }
 
-WXBizDataCrypt.prototype.decryptData = function(encryptedData, iv) {
-  // base64 decode
-  // var sessionKey = new Buffer(this.sessionKey, 'base64')
-  // encryptedData = new Buffer(encryptedData, 'base64')
-  // iv = new Buffer(iv, 'base64')
+WXBizDataCrypt.prototype.decryptData = function (encryptedData, iv) {
   // 转换base64 decode
-  const sessionKey = Buffer.from(this.sessionKey, 'base64');
-  encryptedData = Buffer.from(encryptedData, 'base64');
-  iv = Buffer.from(iv, 'base64');
+  // 使用Buffer.alloc() 、Buffer.allocUnsafe() 、Buffer.from（） 来替代 new Buffer()
+  const sessionKey = Buffer.from(this.sessionKey, 'base64')
+  encryptedData = Buffer.from(encryptedData, 'base64')
+  iv = Buffer.from(iv, 'base64')
   try {
     // 解密
-    const decipher = crypto.createDecipheriv('aes-128-cbc', sessionKey, iv);
+    const decipher = crypto.createDecipheriv('aes-128-cbc', sessionKey, iv)
     // 设置自动 padding 为 true，删除填充补位
-    decipher.setAutoPadding(true);
-    var decoded = decipher.update(encryptedData, 'binary', 'utf8');
-    decoded += decipher.final('utf8');
+    decipher.setAutoPadding(true)
+    var decoded = decipher.update(encryptedData, 'binary', 'utf8')
+    decoded += decipher.final('utf8')
 
-    decoded = JSON.parse(decoded);
+    decoded = JSON.parse(decoded)
   } catch (err) {
-    throw '解密失败，请重试';
+    throw '解密失败，请重试'
   }
   if (decoded.watermark.appid !== this.appId) {
-    throw 'appId不符合，请检查';
+    throw 'appId不符合，请检查'
   }
-  return decoded;
-};
+  return decoded
+}
 
-module.exports = WXBizDataCrypt;
+module.exports = WXBizDataCrypt
